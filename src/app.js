92f3f4a73569ec61ad5c63b3c68cd08671b1dc21
chepy223 <anthonygonzalez.3@hotmail.com>
2021-04-21 19:54:36 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -9,3 +9,41 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let from = document.getElementById("form");
+let card = document.getElementById("card");
+let number = document.getElementById("number");
+let Amount = document.getElementById("Amount");
+let Fname = document.getElementById("Fname");
+let Lname = document.getElementById("Lname");
+let city = document.getElementById("city");
+let Pick = document.getElementById("Pick");
+let postal = document.getElementById("postal");
+
+if (validator.isEmpty(card.value)) {
+  card.style.background = "red";
+}
+
+if (number === "") {
+  number.style.background = "red";
+}
+if (Amount === "") {
+  Amount.style.background = "red";
+}
+if (Fname === "") {
+  Fname.style.background = "red";
+}
+if (Lname === "") {
+  Lname.style.background = "red";
+}
+
+if (city === "") {
+  city.style.background = "red";
+}
+if (Pick === "") {
+  Pick.style.background = "red";
+}
+
+if (postal === "") {
+  postal.style.background = "red";
+}
